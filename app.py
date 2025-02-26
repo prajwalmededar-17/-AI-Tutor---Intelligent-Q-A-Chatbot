3553c23aa8f4150d956614f2ae66e6fe66cedfab
Prajwal_Medadar <prajwalmededar@gmail.com>
2025-02-26 21:56:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,31 @@
-# Q&A Chatbot
-from langchain.llms import OpenAI
-
-#from dotenv import load_dotenv
-
-#load_dotenv()  # take environment variables from .env.
-
+# Q&A Chatbot using Cohere
+import cohere
 import streamlit as st
 import os
 
+# Initialize Cohere with your API key
+cohere_api_key = "B2lU0Zy10rgQ1ys8joMX6HKGPiG7OBdSPecizAvg"
+co = cohere.Client(cohere_api_key)
 
-## Function to load OpenAI model and get respones
+# Function to load Cohere model and get response
+def get_cohere_response(question):
+    response = co.generate(
+        model='command-xlarge-nightly',
+        prompt=question,
+        max_tokens=100,
+        temperature=0.5
+    )
+    return response.generations[0].text.strip()
 
-def get_openai_response(question):
-    llm=OpenAI(model_name="text-davinci-003",temperature=0.5)
-    response=llm(question)
-    return response
-
-##initialize our streamlit app
-
+# Initialize our Streamlit app
 st.set_page_config(page_title="Q&A Demo")
+st.header("Langchain Application with Cohere")
 
-st.header("Langchain Application")
+input_text = st.text_input("Input: ", key="input")
+submit = st.button("Ask the question")
 
-input=st.text_input("Input: ",key="input")
-response=get_openai_response(input)
-
-submit=st.button("Ask the question")
-
-## If ask button is clicked
-
+# If ask button is clicked
 if submit:
-    st.subheader("The Response is")
+    response = get_cohere_response(input_text)
+    st.subheader("The Response is:")
     st.write(response)
-
-
-
-
